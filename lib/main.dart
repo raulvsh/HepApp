c53962a52712215704b864aecacee5aae57cde8c
raulvsh <raulvsh@gmail.com>
2019-07-16 19:34:02 +0200

    Añadidos tabla e iconos de la pantalla principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,31 @@
-import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+import 'vista/SquareHomeWidget.dart';
+//import 'package:flutter_widget_guide/utils.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
+    final title = 'HepApp';
+
     return MaterialApp(
-      title: 'HepApp',
+      debugShowCheckedModeBanner: true, //Quitar etiqueta de debug
+      title: title,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 93, 188, 210),
       ),
-      home: MyHomePage(title: 'HepApp'),
+      home: MyHomePage(title: title),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -46,7 +35,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  final _suggestions = <WordPair>[];
+  bool _isBorderEnabled = false;
+  var _actionIcon = Icons.border_all;
+  final _saved = Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
   List<String> litems = ["1", "2", "Third", "4"];
 
   void _incrementCounter() {
@@ -60,12 +52,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -75,71 +61,48 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-        centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 93, 188, 210),
-      ),
-      body: _buildSuggestions(),
-      /*Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Lidia ha pulsado el botón estas veces',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            Text(
-              wordPair.asPascalCase,
-            ),
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(100),
+          child: PreferredSize(
+            preferredSize: Size.fromHeight(30),
+            child: AppBar(
+              //iconTheme: IconThemeData(color: Colors.red),
+              //shape: BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
 
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-*/
-    );
-  }
+              // Here we take the value from the MyHomePage object that was created by
+              // the App.build method, and use it to set our appbar title.
+              title: Text(
+                widget.title,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontSize: 18,
+                ),
+              ),
+              centerTitle: true,
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(70.0),
+                child: Theme(
+                  data: Theme.of(context).copyWith(accentColor: Colors.white),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      itemBuilder: (context, i) {
-        if (i >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(0));
-        }
-        return _buildRow(_suggestions[i]);
-      },
-    );
-  }
+                        image: AssetImage('/images/goma_borrar.png'),
+                        fit: BoxFit.fitWidth,
 
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(pair.asPascalCase),
+                      ),
+                    ),
+                    height: 60.0,
+                    padding: EdgeInsets.all(3),
+                    alignment: Alignment.center,
+                    //child: Image.asset('images/hepapplogo.png',
+                    //fit: BoxFit.contain),
+                  ),
+                ),
+              ),
+            ),
+          )),
+      body: SquareHomeWidget(),
     );
   }
 }
