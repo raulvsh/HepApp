0c30e40802a71379c4d809747764f3c5bd923e55
raulvsh <raulvsh@gmail.com>
2020-06-02 20:51:17 +0200

    Limpieza de código

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -28,9 +28,7 @@ class _HomePageState extends State<HomePage> {
           child: Stack(
             children: <Widget>[
               HomeAppBar(),
-              //_screenShotButton,
               Container(
-                  //color: Colors.red,
                   margin: EdgeInsets.only(top: 16),
                   height: 20,
                   alignment: Alignment.centerRight,
@@ -44,56 +42,7 @@ class _HomePageState extends State<HomePage> {
         body: CommonGridPage(
           data: homeSections,
         ),
-        /*floatingActionButton: FloatingActionButton(
-          onPressed: takeScreenShot,
-          child: Icon(Icons.add_to_home_screen),
-          mini: true,
-        ),*/
       ),
     );
   }
-
-/* Container get _screenShotButton {
-    AppLocalizations aux = AppLocalizations.of(context);
-
-    return Container(
-      margin: EdgeInsets.only(top: 17),
-      width: context.widthPx,
-      height: 20,
-      alignment: Alignment.centerRight,
-      child: Builder(
-        builder: (context) => IconButton(
-            icon: Icon(
-              Icons.camera_alt,
-              size: 20,
-              color: Colors.white,
-            ),
-            onPressed: () async {
-              await takeScreenShot();
-              Scaffold.of(context).showSnackBar(
-                SnackBar(
-                  backgroundColor: Theme.of(context).primaryColor,
-                  content: Text(aux.tr('screenshot_saved')),
-                ),
-              );
-            }),
-      ),
-    );
-  }*/
-
-/* takeScreenShot() async {
-    await Future<void>.delayed(Duration(milliseconds: 300));
-
-    RenderRepaintBoundary boundary =
-        mainScaffold.currentContext.findRenderObject();
-    ui.Image image = await boundary.toImage(pixelRatio: 3);
-    ByteData byteData = await image.toByteData(
-      format: ui.ImageByteFormat.png,
-    );
-
-    var filePath = await ImagePickerSaver.saveFile(
-      fileData: byteData.buffer.asUint8List(),
-    );
-    print(filePath);
-  }*/
 }
